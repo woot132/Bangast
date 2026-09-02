8edea657873499c7cb93d9afa5cf829ec7492cbe
woot132 <sarawoot120515@gmail.com>
2026-09-02 11:30:42 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbxba2Enfzaz4wu7supLpkW2V-3aQLk4AG6KtPjc6GczcifG4J7wXpcE85pmAZsoKXdBhg/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbxtESKz8WWupskjBIGRG1POgyVyfxHFJCFBfwn3OcQGtrBexidF7pVv6-5mgo67fnMLiA/exec'
   });
 })();
