1cbf5d4675888d8ac0105f72f0e6fc356648c205
woot132 <sarawoot120515@gmail.com>
2026-09-02 16:47:01 +0700

    Add files via upload

--- admin-mode.js
+++ admin-mode.js
@@ -1,8 +1,8 @@
 (() => {
   'use strict';
-  const API_URL=window.APP_CONFIG.API_URL;
-  const CSS_FILES=['edit-website.css?v=20260827-2','news-manager.css?v=20260826-1','newsletter-manager.css?v=20260826-1','newsletter-overlay.css?v=20260826-3','facebook-manager.css?v=20260826-1','admin-subpage.css?v=20260901-index-team-1','team-manager.css?v=20260901-index-team-1'];
-  const JS_FILES=['edit-website.js?v=20260827-2','news-manager.js?v=20260826-1','newsletter-manager.js?v=20260826-4','facebook-manager.js?v=20260826-2','team-manager.js?v=20260901-index-team-1'];
+  const API_URL='https://script.google.com/macros/s/AKfycbxwqpydnQSx2aPrQ8yJAN3P9Jkjic-8nNIlOHYFRyarrtTyb26sE_USzXNS7uk478wh8w/exec';
+  const CSS_FILES=['edit-website.css?v=20260827-2','news-manager.css?v=20260902-newsurl-optional-2','newsletter-manager.css?v=20260826-1','newsletter-overlay.css?v=20260826-3','facebook-manager.css?v=20260826-1'];
+  const JS_FILES=['edit-website.js?v=20260827-2','news-manager.js?v=20260902-newsurl-optional-2','newsletter-manager.js?v=20260826-4','facebook-manager.js?v=20260826-2'];
   let toolsPromise=null;
   let storagePromise=null;
   const STORAGE_CACHE_KEY='mysiteAdminStorageV1';
